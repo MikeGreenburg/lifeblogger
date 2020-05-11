8c0831d09676423f264bacd9898894b8ffc97a91
MichaelGreenburg <m.pgreenburg@gmail.com>
2020-05-11 13:40:54 -0500

    finished the controller exercieses

---
+++ src/main/java/com/codeup/blog/controllers/PostController.java
@@ -0,0 +1,33 @@
+package com.codeup.blog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PostController {
+
+    @GetMapping("/posts")
+    public String allPosts(){
+        return "posts/index";
+    }
+
+    @GetMapping("/posts/{id}")
+    @ResponseBody
+    public String profile(@PathVariable String id){
+        return id;
+    }
+
+    @GetMapping("/posts/create")
+    public String createPost(){
+        return "posts/create";
+    }
+
+    @PostMapping("/posts/show")
+    public String createdPost(){
+        return "posts/show";
+    }
+
+}
